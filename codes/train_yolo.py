ca48f4dd70dd324763e88ec329f06009c5a5ce6e
k1101jh <h1103095@gmail.com>
2023-10-24 15:02:33 +0900

    Yolo 훈련 코드 업데이트

--- codes/train_yolo.py
+++ codes/train_yolo.py
@@ -5,15 +5,34 @@ import torch
 from ultralytics import YOLO
 from tqdm import tqdm
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 ultralytics.checks()
 
 
 def train():
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8m.pt")
     model.to("cuda")
 
-    model.train(data="configs/data.yaml", batch=8, name="yolov8n")
+    model.train(
+        data="configs/data.yaml",
+        name="yolov8m",
+        epochs=1000,
+        patience=200,
+        batch=6,
+        imgsz=800,
+        optimizer="AdamW",
+        lr0=0.001,
+        cos_lr=True,
+        hsv_h=0.015,
+        hsv_s=0.7,
+        hsv_v=0.4,
+        degrees=10,
+        translate=0.2,
+        scale=0.2,
+        # perspective=0.1,
+        fliplr=0.5,
+        mosaic=1.0,
+    )
 
 
 if __name__ == "__main__":
